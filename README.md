8dec8726a64e460b5ae012e7b5e0f802aebddc46
Jason Motylinski <jason@motylinski.com>
2024-03-23 07:19:23 -0400

    feature: added requirements.txt. Implemented dotenv for reading variables from .env file. Added main entry so the functions could be used as a reference lib.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Rawkit Playlist
 automatically creates a playlist on spotify given a dictionary of artist names/songs. Don't forget to set environment variables.
+
+## Requirements
+
+- Application set up in the [Spotify for Developers](https://developer.spotify.com/)
+- The Client ID and Client Secret from the application configured in the Spotify developer portal.
+
+## Setup
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Create a `.env` file to store the required environment variables. the contents of the file should be:
+
+```
+SPOTIPY_CLIENT_ID=....
+SPOTIPY_CLIENT_SECRET=...
+SPOTIPY_REDIRECT_URI=...
+```
+
+
+## Running
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+
 import spotipy
+from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
 
 # authenticating
 scope = "playlist-modify-public user-library-read"
@@ -9,30 +13,26 @@ user_id = spotify.current_user()["id"]
 
 # helper functions
 
-# ASSUMING TRACKS IN A DICT OF [{"artist_name": , "song_name":}]
+
 def compile_track_ids(tracks):
+    # ASSUMING TRACKS IN A DICT OF [{"artist_name": , "song_name":}]
+    # searching for songs
+    # assuming name and track are songs that exist. For some reason Spotipy returns a random song (?) even if the name is misspelled
+
     playlist_ids = []
     for track_info in tracks:
         results = spotify.search(q=f"{ track_info['artist_name'] },{ track_info['song_name'] }")["tracks"]["items"][0]["uri"]
-       
         playlist_ids += [results]
-    
     return playlist_ids
-    
-# searching for songs
-# assuming name and track are songs that exist. For some reason Spotipy returns a random song (?) even if the name is misspelled
 
 def clear_playlist(user,playlist_id):
     spotify.user_playlist_remove_all_occurrences_of_tracks(user=user,playlist_id=playlist_id,tracks=[item["track"]["uri"] for item in spotify.user_playlist_tracks(user=user,playlist_id=playlist_id)["items"]])
 
-
 def create_playlist(user): 
     user_playlists = spotify.user_playlists(user=user, limit=50, offset=0)["items"]
     for playlist in user_playlists:
-        if playlist["name"] == "rawkit":
-            
+        if playlist["name"] == "rawkit":            
             return playlist["id"]
-
  
     spotify.user_playlist_create(user=user,name="rawkit", description="automatically updating playlist with the top 10 songs from rawkit.com")
     create_playlist(user)
@@ -47,5 +47,5 @@ def update_playlist(songs):
     # adding the tracks ids of the songs to the rawkit playlist
     spotify.user_playlist_add_tracks(user=user_id,playlist_id=playlist_id,tracks=compile_track_ids(songs))
 
-
-update_playlist([{"artist_name":"radiohead","song_name" : "weird fishes"}])
+if __name__ == "__main__":
+    update_playlist([{"artist_name":"radiohead","song_name" : "weird fishes"}])
